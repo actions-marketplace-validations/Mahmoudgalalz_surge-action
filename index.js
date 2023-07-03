2916829a8401f782c58d3be05c3882ab8a69d178
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-07-04 01:00:19 +0300

    init commit with testing on action

---
+++ index.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const options ={};
+
+async function setup(){
+    try{
+        await exec.exec('npm i -g surge')
+        const token =  core.getInput('auth-token')
+        await exec.exec(`export SURGE_TOKEN=${token}`)
+    }
+    catch(err){
+        core.setFailed(err.message)
+    }
+}
+
+async function run(){
+    try{
+        const domain = core.getInput('domain')
+        const path = core.getInput('path')
+        await setup();
+        let output = ''
+        let errors = ''
+
+        options.listeners = {
+            stdout:(data)=>{
+                output+=data.toString()
+            },
+            stderr:(data)=>{
+                errors+=data.toString()
+            }
+        }
+        await exec.exec('surge',[`${path}`,'--domain',`${domain}`],options)
+        core.setOutput('domain',output)
+    }
+    catch(err){
+        core.setFailed(err.message)
+    }
+}
+
+run()
+
